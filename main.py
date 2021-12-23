6032c9e32521c863722bd490ca338cd9b88ffadb
shlomobamberger <b0533129539@gmail.com>
2021-12-23 14:23:19 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,14 @@ import feedparser
 import requests
 import os
 import re
+import winsound
+
+http_proxy = "http://proxy-chain.intel.com:911"
+
+proxyDict = {
+    "http": http_proxy,
+    "https": http_proxy,
+}
 
 
 def title_and_posts_from_rss(rss_file):
@@ -13,17 +21,17 @@ def data_from_post(post):
     return {'link': post.enclosures[0].href, 'title': post.title}
 
 
-def remove_special_char(text):
+def remove_spacial_char(text):
     return re.sub('\W+', ' ', text)
 
 
 def main():
-    rss_link = ''
+    rss_link = 'https://www.podcasti.co/minisites/cyber/feed.xml'
     if not rss_link:
         rss_link = input("please enter rss link: ")
-    rss_file = requests.get(rss_link).content
+    rss_file = requests.get(rss_link, proxies=proxyDict).content
     all_post_data = title_and_posts_from_rss(rss_file)
-    title = remove_special_char(all_post_data["title"])
+    title = remove_spacial_char(all_post_data["title"])
     posts = all_post_data["posts"]
     print('Starts with a download of ', len(posts), 'episodes from the podcast ' + title)
     if not os.path.exists(title):
@@ -31,10 +39,12 @@ def main():
         print("A new folder " + title + "  has been created")
     for post in posts:
         episode = data_from_post(post)
-        file_path = title + '\\' + remove_special_char(episode['title']) + '.mp3'
-        mp3 = requests.get(episode['link'])
-        with open(file_path, 'wb') as f:
-            f.write(mp3.content)
+        file_path = title + '\\' + remove_spacial_char(episode['title']) + '.mp3'
+        if not os.path.exists(file_path):
+            mp3 = requests.get(episode['link'], proxies=proxyDict)
+            with open(file_path, 'wb') as f:
+                f.write(mp3.content)
+        print(f"There are {len(posts) - posts.index(post) - 1} episodes left out of {len(posts)}")
     print('DONE!')
 
 
